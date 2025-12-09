3c8c0771081972729b8134d1aa400b8f18c1bc31
AngelicaSjodin <angelic.sjodin@gmail.com>
2025-12-09 04:47:58 +0100

    component stuff (borde vara connected nu)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
+import { IonicModule } from '@ionic/angular';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, 
+    IonRouterOutlet, 
+    CommonModule, 
+    IonicModule, 
+    HttpClientModule,
+    RouterModule],
 })
 export class AppComponent {
   constructor() {}
