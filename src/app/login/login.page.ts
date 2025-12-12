09099826bf7d135f110872444f3bdd5ceebd7ccc
AngelicaSjodin <angelic.sjodin@gmail.com>
2025-12-12 22:26:50 +0100

    start på login.page.ts

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import {IonicModule} from '@ionic/angular';
+
+@Component({
+    standalone: true,
+    selector: 'app-login',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
+    imports: [IonicModule]
+})
+export class LoginPage {
+    constructor(private router: Router) {}
+
+    login(){
+        this.router.navigate(['/home']);
+    }
+}
