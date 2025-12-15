9312c90ff8663478eae775321beb5b6c252652e6
AngelicaSjodin <angelic.sjodin@gmail.com>
2025-12-15 18:36:02 +0100

    create accoutn page ts, lägger till vart ionic hämtar functionen från spring

--- src/app/createAccount/createAccount.page.ts
+++ src/app/createAccount/createAccount.page.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from "@angular/common/http";
 import { Component } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 import { Router } from "@angular/router";
@@ -15,9 +16,25 @@ export class CreateAccountPage {
     name: string ="";
     password: string ="";
 
-    constructor(private router: Router) {}
+    constructor(private router: Router,
+        private http: HttpClient
+    ) {}
 
     createAccount(){
-        this.router.navigate(['/login']);
+        const user = {
+            name: this.name,
+            password: this.password
+        };
+
+        this.http.post('http://localhost:8080/users/register',user).subscribe({
+            next: () => {
+                alert('konto skapat');
+                this.router.navigate(['/login']);
+            },
+            error: err => {
+                console.error(err);
+                alert('error, kunde inte skapa konto');
+            }
+        });
     }
 }
