b4cf08e4f12f227a0f36f7ad68b093916fe796d5
AngelicaSjodin <angelic.sjodin@gmail.com>
2025-12-16 22:52:29 +0100

    MISSADE LÄNKA I HTML AAA

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,22 +1,42 @@
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 import {IonicModule} from '@ionic/angular';
+import { HttpClient } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { AuthService } from "../services/auth.service";
 
 @Component({
     standalone: true,
     selector: 'app-login',
     templateUrl: './login.page.html',
     styleUrls: ['./login.page.scss'],
-    imports: [IonicModule]
+    imports: [IonicModule,FormsModule]
 })
 export class LoginPage {
-    constructor(private router: Router) {}
 
-    login(){
-        this.router.navigate(['/loggedIn']);
+    name: string ='';
+    password: string ='';
+
+    constructor(private router: Router,private auth: AuthService,private http: HttpClient) {}
+
+    login(): void{
+
+        this.http.post<{message: string}>(
+            'http://localhost:8080/users/login',
+            {name: this.name, password: this.password}
+        ).subscribe({
+            next:(res)=>{
+                console.log("ok:",res.message);
+                this.auth.login();
+                this.router.navigateByUrl('/loggedIn');
+            },
+            error: (err: any) => {
+                console.log("fail:",err.error?.message ?? err);
+            }
+        });
     }
 
-    create(){
+    create(): void{
         this.router.navigate(['/createAccount']);
     }
 }
